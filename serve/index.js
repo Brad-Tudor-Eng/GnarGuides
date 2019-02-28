361b9cb1813e14ad9b1cf1e46c4746b65b321012
Brad-Tudor-Eng <brad.tudor.eng@gmail.com>
2019-02-27 18:01:39 -0800

    added env and compression

--- serve/index.js
+++ serve/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const compression = require('compression')
 const app = express()
 const path = require('path')
+require('dotenv').config()
 
 app.use(express.static(path.join(__dirname, "client")))
+app.use(compression())
 
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname, "client", "index.html"))
